f9ec254ee074115a692bbb93caa33fe68e41b241
mirzohidd <mirzohidmurodov77@gmail.com>
2024-11-14 19:19:59 +1000

    feat:develop author router ejs  page

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 console.log("Web Serverni boshlash");
 
 const express = require("express");
+// const res = require("express/lib/response");
 const app = express();
 const http = require("http");
+const fs = require("fs");
 
+let user;
+fs.readFile("database/user.json","utf-8",(err,data)=>{
+    if(err){
+        console.log("ERROR:",err)
+    }else{
+        user = JSON.parse(data)
+    }
+})
 // 1 Kirish code
 app.use(express.static("public"));
 app.use(express.json());
@@ -15,12 +25,17 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 Routing code
-app.get("/hello", function (req, res) {
-  res.end("Hello world by Julian");
+app.post("/create-item",(req, res)=>{
+    console.log(req.body);
+    res.json({test:"success"});
+})
+app.get("/", function (req, res) {
+    res.render(`harid`);
 });
-app.get("/gift", function (req, res) {
-  res.end(`<h1>Siz sovg'alar sahifasidasiz</h1>`);
-});
+app.get("/author",(req,res)=>{
+    res.render("author",{user:user})
+})
+  
 
 const server = http.createServer(app);
 let PORT = 3001;
