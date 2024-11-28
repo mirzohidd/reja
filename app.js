02e1e770f8e848cf0f645fcf3586566c17f3f920
mirzohidd <mirzohidmurodov77@gmail.com>
2024-11-28 21:37:48 +1000

    feat:fix delete bug

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 //  MongoDB connect
 const db = require("./server").db();
-
+const mongodb = require("mongodb");
 // 1 Kirish code
 app.use(express.static("public"));
 app.use(express.json());
@@ -25,9 +25,18 @@ app.post("/create-item", (req, res) => {
   console.log(req.body);
   const new_reja = req.body.reja;
   db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
-    console.log(data.ops)
-  //  res.json(data.ops[0]);
+    // console.log(data.ops)
+    res.json(data.ops[0]);
   });
+});
+app.post("/delete-item", (req, res) => {
+  const id = req.body.id;
+  db.collection("plans").deleteOne(
+    { _id: new mongodb.ObjectId(id) },
+    function (err, data) {
+      res.json({ state: "success" });
+    }
+  );
 });
 app.get("/", function (req, res) {
   console.log("user entered /");
--- public/browser.js
+++ public/browser.js
@@ -1,17 +1,15 @@
 console.log("FronEnd js ishga Tushdi");
 function itemTemplate(item) {
   return `<li>
-					<span class="task-text"> ${item.reja}</span>
-					<div>
-					  
-					  <button data-id=" ${item._id}" class="btn edit-btn">Edit</button>
-					  <button data-id=" ${item._id} " class="btn delete-btn">
-						Delete
-					  </button>
-					</div>
-				  </li>`;
+    <span class="task-text">${item.reja}</span>
+    <div>
+      <button data-id="${item._id}" class="btn edit-btn">Edit</button>
+      <button data-id="${item._id}" class="btn delete-btn">Delete</button>
+    </div>
+  </li>`;
 }
 let createField = document.getElementById("create-field");
+// console.log()
 document.getElementById("create-form").addEventListener("submit", function (e) {
   e.preventDefault();
 
@@ -21,10 +19,38 @@ document.getElementById("create-form").addEventListener("submit", function (e) {
       document
         .getElementById("item-list")
         .insertAdjacentHTML("beforeend", itemTemplate(response.data));
-        createField.value ="";
-        createField.focus();
+      createField.value = "";
+      createField.focus();
     })
     .catch((err) => {
-        console.log("iltimos qaytdan urinib koring")
+      console.log("iltimos qaytdan urinib koring");
     });
+});
+
+document.addEventListener("click", function (e) {
+  //   e.preventDefault();
+
+  // delete operation
+  if (e.target.classList.contains("delete-btn")) {
+    e.preventDefault();
+
+    if (confirm("Are you sure delete this goal?")) {
+      axios
+        .post("delete-item", { id: e.target.getAttribute("data-id") })
+        .then((response) => {
+          console.log(response.data);
+          e.target.parentElement.parentElement.remove();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }
+  // edeit operation
+
+  if (e.target.classList.contains("edit-btn")) {
+    e.preventDefault();
+
+    alert("Siz edit knopkasini bosdiz");
+  }
 });
